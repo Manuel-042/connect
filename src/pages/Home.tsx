c8e0b8d5fb63d39ad5bbe28ac67a6af6645bd664
Ebuka Ezeanya <ebukaezeanya14@gmail.com>
2024-08-15 16:19:20 +0100

    Added 404 and home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1 className="text-green-600 font-bold">Hello, first time tailwind user</h1>
+    )
+}
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,6 @@
+
+export default function NotFound() {
+    return (
+        <h1>Error 404! what are you looking for buddy?</h1>
+    )
+}
